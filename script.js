38f200942dd5bb5415f47828b2d0c9daa72110b0
Nicolas Lorenzo <nicolaas.lorenzao@escola.pr.gov.br>
2025-04-24 09:02:25 -0300

    Adicionando Menu

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
         hamburger.classList.toggle('active');
         navLinks.classList.toggle('active');
     });
+    document.addEventListener('DOMContentLoaded', function () {
+
+        const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+        const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
+        let tamanhoAtualFonte = 1;
+    
+        aumentaFonteBotao.addEventListener('click', function () {
+            tamanhoAtualFonte += 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
+        })
+    
+        diminuiFonteBotao.addEventListener('click', function () {
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
+        })
+    
+    });
 
     document.querySelectorAll('.nav-link').forEach(link => {
         link.addEventListener('click', () => {
